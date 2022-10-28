852d432cac274d01c3462104eb9733e80156761c
tzx303 <114803366+tzx303@users.noreply.github.com>
2022-10-28 16:25:11 +0800

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -1,3 +1,31 @@
 import socket
 
-print(socket.gethostbyname(socket.gethostname()))
+sockfd = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+sockfd.bind(('0.0.0.0',12345))
+while True:
+    sockfd.listen(5)
+
+    print('waiting')
+    try:
+        connfd,addr = sockfd.accept()
+
+        print('connect from',addr)
+    except KeyboardInterrupt:
+        print('exit')
+
+    while True:
+        data = connfd.recv(1024)
+        if not data:
+            break
+        print("收到：",data.decode())
+        m = 'thank'
+
+        n = connfd.send(m.encode())
+
+        print('发送%d'%n)
+    connfd.close()
+
+sockfd.close()
+
+
